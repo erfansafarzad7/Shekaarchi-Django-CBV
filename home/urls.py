d0a00af2d2b96e136b9a3284b2db0b070562e4e5
erfansafarzad7 <erfansafarzad7@gmail.com>
2023-01-26 18:42:20 +0330

    Login-Logout

--- home/urls.py
+++ home/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'accounts'
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
-
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,20 +1,21 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from files.models import Item
+from django.contrib.auth import views as auth_view
+from django.urls import reverse_lazy
 
 
 class HomeView(ListView):
     template_name = 'home/home.html'
-    # model = Item
-    # queryset = Car.objects.filter(year__gte=2000)
+    queryset = Item.objects.all()
     context_object_name = 'items'
-    # ordering = 'year'
 
-    def get_queryset(self):
-        return Item.objects.all()
 
-    # def get_context_data(self, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     context['username'] = 'jack'
-    #     return context
+class LoginView(auth_view.LoginView):
+    template_name = 'home/login.html'
+    next_page = reverse_lazy('home:home')
+
+
+class LogoutView(auth_view.LogoutView):
+    next_page = reverse_lazy('home:home')
 
