c82df20ae13555707089d7fafdce87d618efb40c
erfansafarzad7 <erfansafarzad7@gmail.com>
2023-01-31 19:25:59 +0330

    Register

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,12 @@
 from django.contrib.auth import views as auth_view
 from django.urls import reverse_lazy
-from django.contrib.messages import views as message
+from django.contrib.messages.views import SuccessMessageMixin
+from django.views.generic import CreateView
+from .models import User
+from .forms import UserCreationForm
 
 
-class LoginView(message.SuccessMessageMixin, auth_view.LoginView):
+class LoginView(SuccessMessageMixin, auth_view.LoginView):
     """
     Login and send success message.
     """
@@ -17,3 +20,14 @@ class LogoutView(auth_view.LogoutView):
     Logout view.
     """
     next_page = reverse_lazy('home:home')
+
+
+class RegisterView(SuccessMessageMixin, CreateView):
+    """
+    Register new user.
+    """
+    template_name = 'accounts/register.html'
+    form_class = UserCreationForm
+    success_message = 'User Successfully Created!'
+    success_url = reverse_lazy('accounts:login')
+
