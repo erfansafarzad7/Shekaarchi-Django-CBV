00f866fa89f9faf81fc2ae526f4bd14f07a4f03f
erfansafarzad7 <erfansafarzad7@gmail.com>
2023-02-01 16:43:43 +0330

    AutoLoginAfterRegister

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,14 @@
-from django.contrib.auth import views as auth_view
-from django.urls import reverse_lazy
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.views import LoginView, LogoutView
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy, reverse
 from django.contrib.messages.views import SuccessMessageMixin
 from django.views.generic import CreateView
 from .models import User
 from .forms import UserCreationForm
 
 
-class LoginView(SuccessMessageMixin, auth_view.LoginView):
+class LoginView(SuccessMessageMixin, LoginView):
     """
     Login and send success message.
     """
@@ -15,7 +17,7 @@ class LoginView(SuccessMessageMixin, auth_view.LoginView):
     next_page = reverse_lazy('home:home')
 
 
-class LogoutView(auth_view.LogoutView):
+class LogoutView(LogoutView):
     """
     Logout view.
     """
@@ -29,5 +31,12 @@ class RegisterView(SuccessMessageMixin, CreateView):
     template_name = 'accounts/register.html'
     form_class = UserCreationForm
     success_message = 'User Successfully Created!'
-    success_url = reverse_lazy('accounts:login')
+    success_url = reverse_lazy('home:home')
+
+    def form_valid(self, form):
+        valid = super(RegisterView, self).form_valid(form)
+        username, password = form.cleaned_data.get('username'), form.cleaned_data.get('password1')
+        new_user = authenticate(username=username, password=password)
+        login(self.request, new_user)
+        return valid
 
