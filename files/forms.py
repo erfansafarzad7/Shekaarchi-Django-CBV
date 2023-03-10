ab33707ffdca9b456f0f00b3191ae12c0278f610
erfansafarzad7 <erfansafarzad7@gmail.com>
2023-03-10 16:43:32 +0330

    --CodeUpdate+requirements.txt

--- files/forms.py
+++ files/forms.py
@@ -1,7 +1,5 @@
 from django import forms
 from .models import Item
-from accounts.models import User
-from django.forms import inlineformset_factory
 
 
 class ItemCreateForm(forms.ModelForm):
@@ -25,17 +23,5 @@ class ItemUpdateForm(forms.ModelForm):
     class Meta:
         model = Item
         fields = '__all__'
-        exclude = ('user', 'code')
-
-
-# class ItemImageForm(forms.ModelForm):
-#
-#     class Meta:
-#         model = ItemImage
-#         fields = '__all__'
-
-#
-# ItemImageFormSet = inlineformset_factory(
-#     Item, ItemImage, form=ItemImageForm, fields=['image'], extra=3, can_delete=True
-# )
+        exclude = ('user', 'code', 'images')
 
